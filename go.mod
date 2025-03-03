7fb587f81ed9ad8f1a1d14068a3a48a31e3a6dd5
chen.lili <chen.lili@jcbc.co.jp>
2025-03-03 16:22:04 +0900

    import mDNS protocol to implement node discovery

--- go.mod
+++ go.mod
@@ -2,10 +2,22 @@ module weather-blockchain
 
 go 1.24
 
-require github.com/stretchr/testify v1.10.0
+require (
+	github.com/grandcat/zeroconf v1.0.0
+	github.com/hashicorp/mdns v1.0.6
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/miekg/dns v1.1.55 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -3,10 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"net"
 	"testing"
 	"time"
 )
+
+// MockMDnsServerServer is a mock implementation of the zeroconf.Server interface
+type MockMDnsServerServer struct {
+	mock.Mock
+}
+
+func (m *MockMDnsServerServer) Shutdown() error {
+	m.Called()
+	return nil
+}
 
 // TestServerStart tests if the node starts successfully
 func TestServerStart(t *testing.T) {
@@ -56,9 +67,19 @@ func TestServerStop(t *testing.T) {
 	err := node.Start()
 	assert.NoError(t, err, "Failed to start node")
 
+	// Create a mock server
+	mockServer := new(MockMDnsServerServer)
+	mockServer.On("Shutdown").Return()
+
+	// Inject the mock
+	node.server = mockServer
+
 	// Stop the node
 	err = node.Stop()
 	assert.NoError(t, err, "Failed to stop node")
+
+	// Verify the mock was called
+	mockServer.AssertCalled(t, "Shutdown")
 
 	// Check if node is not listening anymore
 	assert.False(t, node.IsListening(), "Server should not be listening after stop")
@@ -66,4 +87,124 @@ func TestServerStop(t *testing.T) {
 	// Try to connect to the node, should fail
 	_, err = net.DialTimeout(TcpNetwork, "localhost:8002", 500*time.Millisecond)
 	assert.Error(t, err, "Server is still accepting connections after stop")
+
+}
+
+// TestTwoNodesDiscovery tests the actual discovery of two nodes
+func TestTwoNodesDiscovery(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
+
+	// Start two nodes
+	id1 := "test-two-node-1"
+	port1 := 8011
+	id2 := "test-two-node-2"
+	port2 := 8012
+	node1 := NewNode(id1, port1)
+	node2 := NewNode(id2, port2)
+
+	var err error
+	err = node2.Start()
+	assert.NoError(t, err, "Starting node2 should not error")
+	defer node2.Stop()
+
+	err = node1.Start()
+	assert.NoError(t, err, "Starting node1 should not error")
+	defer node1.Stop()
+
+	// Wait for discovery to happen
+	t.Log("Waiting for node discovery...")
+	time.Sleep(2 * MDNSDiscoverInterval)
+
+	// Check if nodes discovered each other
+	node1Discovered := false
+	for id := range node2.GetKnownNodes() {
+		if id == id1 {
+			node1Discovered = true
+			break
+		}
+	}
+
+	node2Discovered := false
+	for id := range node1.GetKnownNodes() {
+		if id == id2 {
+			node2Discovered = true
+			break
+		}
+	}
+
+	// This test might fail depending on network environment
+	assert.True(t, node1Discovered, "Node 1 should be discovered")
+	assert.True(t, node2Discovered, "Node 2 should be discovered")
+}
+
+func TestThreeNodesDiscovery(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
+
+	// Start three nodes
+	id1 := "test-three-node-1"
+	port1 := 18021
+	id2 := "test-three-node-2"
+	port2 := 18022
+	id3 := "test-three-node-3"
+	port3 := 18023
+	node1 := NewNode(id1, port1)
+	node2 := NewNode(id2, port2)
+	node3 := NewNode(id3, port3)
+
+	var err error
+	err = node1.Start()
+	assert.NoError(t, err, "Starting node1 should not error")
+	defer node1.Stop()
+
+	err = node2.Start()
+	assert.NoError(t, err, "Starting node2 should not error")
+	defer node2.Stop()
+
+	err = node3.Start()
+	assert.NoError(t, err, "Starting node3 should not error")
+	defer node3.Stop()
+
+	// Wait for discovery to happen
+	t.Log("Waiting for node discovery...")
+	time.Sleep(2 * MDNSDiscoverInterval)
+
+	node2DiscoveredByNode1 := false
+	node3DiscoveredByNode1 := false
+	for id := range node1.GetKnownNodes() {
+		if id == id2 {
+			node2DiscoveredByNode1 = true
+		} else if id == id3 {
+			node3DiscoveredByNode1 = true
+		}
+	}
+	assert.True(t, node2DiscoveredByNode1, "Node 2 should be discovered by node 1")
+	assert.True(t, node3DiscoveredByNode1, "Node 3 should be discovered by node 1")
+
+	node1DiscoveredByNode2 := false
+	node3DiscoveredByNode2 := false
+	for id := range node2.GetKnownNodes() {
+		if id == id1 {
+			node1DiscoveredByNode2 = true
+		} else if id == id3 {
+			node3DiscoveredByNode2 = true
+		}
+	}
+	assert.True(t, node1DiscoveredByNode2, "Node 1 should be discovered by node 2")
+	assert.True(t, node3DiscoveredByNode2, "Node 3 should be discovered by node 2")
+
+	node1DiscoveredByNode3 := false
+	node2DiscoveredByNode3 := false
+	for id := range node3.GetKnownNodes() {
+		if id == id1 {
+			node1DiscoveredByNode3 = true
+		} else if id == id2 {
+			node2DiscoveredByNode3 = true
+		}
+	}
+	assert.True(t, node1DiscoveredByNode3, "Node 1 should be discovered by node 3")
+	assert.True(t, node2DiscoveredByNode3, "Node 2 should be discovered by node 3")
 }
